f7cf214eca3ee0f8557a2f938a357c3d47b5efde
Tod Fleeman <rtoddf@me.com>
2017-08-18 12:32:43 -0400

    added browserify

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,31 @@
-var gulp = require('gulp');
-	gutil = require('gulp-util');
+var gulp = require('gulp'),
+	gutil = require('gulp-util'),
+	coffee = require('gulp-coffee'),
+	concat = require('gulp-concat'),
+	browserify = require('gulp-browserify');
 
-gulp.task('log', function(){
-	gutil.log('workflows are awesome');
+var coffeeSrcs = ['components/coffee/tagline.coffee'],
+	coffeeDest = 'components/scripts',
+	jsSrcs = [
+		'components/scripts/tagline.js',
+		'components/scripts/template.js'
+	]
+
+// gulp.task('log', function(){
+// 	gutil.log('workflows are awesome');
+// });
+
+gulp.task('coffee', function(){
+	gulp.src(coffeeSrcs)
+		.pipe(coffee({ bare: true })
+			.on('error', gutil.log)
+		)
+		.pipe(gulp.dest(coffeeDest))
+})
+
+gulp.task('js', function(){
+	gulp.src(jsSrcs)
+		.pipe(concat('script.js'))
+		.pipe(browserify())
+		.pipe(gulp.dest('builds/development/scripts'))
 });
