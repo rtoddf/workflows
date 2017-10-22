369aea4b3a1b3df36bfe4e2402b1f931dff54c35
Tod Fleeman <rtoddf@me.com>
2017-10-22 13:39:50 -0400

    learned maps and sets

--- components/scripts/es6.js
+++ components/scripts/es6.js
@@ -1,3 +1,50 @@
+// maps
+var course = new Map();
+
+course.set('react', {description: 'ui'});
+course.set('jest', {description: 'testing'});
+
+console.log(course);
+console.log(course.react);
+console.log(course.get('react'));
+
+var details = new Map([
+	[new Date(), 'today'],
+	['items', [1, 2]]
+]);
+
+console.log(details.size);
+details.forEach(function(item){
+	console.log(item);
+});
+
+
+//sets - colletions of values, but each must be unique
+//duplicates are removed
+var books = new Set();
+books.add('It')
+	.add('The Stand')
+	.add('Misery');
+
+console.log('books size: ', books.size);
+console.log('has It: ', books.has('It'));
+
+//for of loop
+var topics = new Map([
+	['HTML', '/class/html'],
+	['CSS', '/class/css'],
+	['Javascript', '/class/javascript'],
+	['Node', '/class/node']
+]);
+
+for(let topic of topics.keys()){
+	console.log('key: ', topic)
+}
+
+for(let topic of topics.values()){
+	console.log('value: ', topic)
+}
+
 // $ npm install --save-dev gulp-babel babel-preset-env
 // npm install --save-dev babel-core
 // https://www.npmjs.com/package/gulp-babel
@@ -18,10 +65,10 @@
 // 	document.getElementsByTagName('section')[0].appendChild(div);
 // }
 
-function print(firstName){
-	console.log(`hello ${firstName}`);
-}
+// function print(firstName){
+// 	console.log(`hello ${firstName}`);
+// }
 
-print('Todder');
+// print('Todder');
 
 // Array.from(document.getElementsByTagName('div')).forEach(x => console.log(`damn ${x} `));
